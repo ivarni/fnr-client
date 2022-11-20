c9c4f2644a79db3e47cfdb9706d5958d8caa8fb4
Ivar Nilsen <post@ivarnilsen.com>
2022-11-20 19:18:30 +0100

    Try to kill zombie serviceworker...

--- next.config.js
+++ next.config.js
@@ -2,6 +2,30 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [{
+          key: 'Clear-Site-Data',
+          value: '"storage"'
+        }]
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [{
+          type: 'header',
+          key: 'Service-Worker',
+        }],
+        destination: '/kill-sw.js',
+        permanent: false,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
